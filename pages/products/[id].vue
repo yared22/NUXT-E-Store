d2275ba78a0b395c10e81cc007322910ca49a980
yared22 <yarednegassi@gmail.com>
2025-07-22 08:39:42 +0300

    fix: improve product details display in product page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,6 +1,8 @@
 <template>
   <div class="max-w-2xl mx-auto my-10 p-5 bg-gray-50 rounded-lg shadow-lg font-sans text-gray-700">
-    <p>{{ product.title }}</p>
+    <p>{{ product.title }}</p><br>
+    <p>About the product:{{ product.description }}</p><br>
+    <p>prize:{{ prize }}</p>
      <NuxtLink to="/" class="text-blue-500 no-underline font-semibold transition-colors duration-300 hover:text-blue-700 hover:underline">Home</NuxtLink>
   </div>
 </template>
